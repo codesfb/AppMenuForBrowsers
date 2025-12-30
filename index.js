d67f681b2fd50182d39d5c865c5db0f0a6002c79
codesfb <fabio.santana950@gmail.com>
2025-12-30 20:17:23 -0300

    lots of changes now is even usable

--- index.js
+++ index.js
@@ -15,62 +15,222 @@ function el(tag, atributs={}, ...children){
 }
 
 
-listaApps = {}
 const containerAdd = document.querySelector(".containerAddApps");
 const containerApps = document.querySelector(".containerApps");
-function addApp(){
-    const card = el("div",{class:"card",
-        style:{padding: "20px",
-            borderRadius: "8px",
-            boxShadow: "0 2px 5px rgba(0,0,0,0.2)"} },
-        el("div",{},
-        el("p",{},"Add a app or website here"),
-        el("input",{placeholder:"Ex:Youtube.com"}, ),
-        el("button",{id:"btn-submit"},"add")
-    ))
-    return card 
+
+
+function eUmLinkValido(text){
+    const padrao = /^(https?:\/\/)?([\w\d-]+\.)+\w{2,}(\/.*)?$/;
+    return padrao.test(text);
+    
 }
+
+
+
+function extrairNomeLimpo(url) {
+
+    let nome = url.replace(/^https?:\/\//, "").replace("www.", "");
+    nome = nome.split('.')[0];
+    return nome.charAt(0).toUpperCase() + nome.slice(1);
+}
+
+
+let modoRemocao = false;
+
+function alternarAvisoRemocao(mostrar) {
+    const container = document.querySelector(".containerApps");
+    const avisoId = "aviso-remocao";
+    let aviso = document.getElementById(avisoId);
+
+    if (mostrar) {
+        if (!aviso) {
+            aviso = el("div", { 
+                id: avisoId, 
+                style: { background: "#ff4444", color: "white", padding: "10px", textAlign: "center", borderRadius: "5px", marginBottom: "10px" } 
+            }, "Select a website  to remove");
+            container.append(aviso); 
+        }
+    } else if (aviso) {
+        aviso.remove();
+    }
+}
+
+
+function removeApp(urlParaRemover) {
+    let links = JSON.parse(localStorage.getItem("mylinks")) || [];
+    links = links.filter(site => site.url !== urlParaRemover);
+    localStorage.setItem("mylinks", JSON.stringify(links));
+    renderizaApps();
+}
+
+
+        
+
+function mostrarModalConfirmacao(url) {
+    const modal = el("div", { class: "containerAddApps active", style:{padding: "20px",
+            borderRadius: "8px",
+            boxShadow: "0 2px 5px rgba(0,0,0,0.2)"}
+ }, 
+        el("div", { class: "card" },
+            el("p", {style: { color: "gray"}}, `Are you sure you want do remove ${url}?`),
+            el("button", { 
+                style: {border: "none",
+                     background: "#e75c5c",
+                     borderRadius: "8px", 
+                     color: "white",
+                    padding: "12px" },
+                onclick: () => {
+                    removeApp(url);
+                    modal.remove();
+                    desativarModoRemocao();
+                }
+            }, "yes, remove"),
+            el("button", {style:{border: "none",
+                     background: "#808080ff",
+                     borderRadius: "8px", 
+                     color: "white",
+                    padding: "12px",
+                    margin:"12px"        
+                }, 
+                onclick: () => modal.remove() 
+            }, "Cancel")
+        )
+    );
+    document.body.append(modal);
+}
+
+
+function salvaAppLocal(newlink){
+    const links = JSON.parse(localStorage.getItem("mylinks")) || [];
+    links.push(newlink)
+    localStorage.setItem("mylinks", JSON.stringify(links));
+}
+
+function renderizaApps(){
+    const container = document.querySelector(".containerApps");
+    const sites = JSON.parse(localStorage.getItem("mylinks")) || [];
+    container.textContent=""
+
+    if(sites.length===0){
+        container.append(el("p",{style: { color: "gray"}},"You dont have any apps yet please add them and enjoy :)"));
+        return    
+    }
+    sites.forEach(site => {
+     const card = el("div", { class: "app-card" },
+                                el("a", { href: `https://${site.url}`, target: "_blank" },
+                                el("img", { src: site.logo }),
+                                el("p", {}, extrairNomeLimpo(site.url))
+                                )
+                            );
+            
+
+            card.onclick = (e) => {
+            if (modoRemocao) {
+                e.preventDefault(); 
+                mostrarModalConfirmacao(site.url);
+            } else {
+                window.open(`https://${site.url}`, "_blank");
+            }
+        };                    
+
+        container.append(card);
+
+        });
+
+}
+
+
+
+
+function addApp() {
+    const inputField = el("input", { placeholder: "Ex: youtube.com", class: "modal-input" });
+
+    const card = el("div", { class: "card",  style:{padding: "20px",
+            borderRadius: "8px",
+            boxShadow: "0 2px 5px rgba(0,0,0,0.2)"}},
+        el("div", {},
+            el("p", {}, "Add a app or website here"),
+            inputField,
+            el("button", {
+                id: "btn-submit",
+                onclick: () => {
+
+
+
+                        
+                            let valor = inputField.value.trim().toLowerCase();
+
+                            if (valor === "") return;
+
+                            if (!valor.includes(".")) {
+                                valor += ".com";
+                            }
+                            /*
+                           if (!valor.includes("://")) {
+                                valor = "https://" + valor;
+                            }
+                              */  
+                            app(valor);
+                            containerAdd.classList.remove("active");
+                            document.querySelector(".containerAddApps").textContent = "";
+
+                }
+            }, "Add")
+        )
+    );
+
+    return card;
+}
+
+
 
 
 function app(link){
-   
+    const dominioPuro = link.replace(/^https?:\/\//, "")
     const novoApp = {
          url:link,
-         logo:`https://www.google.com/s2/favicons?domain=${link}&sz=64`
+         logo:`https://www.google.com/s2/favicons?domain=${dominioPuro}&sz=64`
     }
-
-    //salvaApp(newApp)
-
-    const appVisual = el("div", { class: "app-card" },
-        el("a", { href: `https://${novoApp.url}`, target: "_blank" },
-            el("img", { src: novoApp.logo }),
-            el("p", {}, novoApp.url)
-        )
-    );
-    containerApps.append(appVisual);
-
+    salvaAppLocal(novoApp);
+    renderizaApps(); 
+    
 }
 
 const btn = document.querySelector("#btn-add");
-
-
-
 btn.addEventListener("click", ()=>{
+        containerAdd.classList.add("active");
         const add = addApp();
        
-        containerAdd.append(add);
-
-        const btnAdd = document.querySelector("#btn-submit");
-        btnAdd.addEventListener("click", (e)=>{
-        
-        const input = e.target.parentElement.querySelector("input");
-        console.log(input.value);
-        app(input.value);
-        containerAdd.textContent="";
-
-    });
+        containerAdd.prepend(add);
 
         
+
+        
+       
 
 });
 
+
+
+const btnRemoverModo = document.querySelector("#btn-remove"); 
+
+btnRemoverModo.addEventListener("click", () => {
+    modoRemocao = !modoRemocao;
+    
+    if (modoRemocao) {
+        btnRemoverModo.textContent = "Cancel Remove";
+        alternarAvisoRemocao(true);
+    } else {
+        desativarModoRemocao();
+    }
+});
+
+function desativarModoRemocao() {
+    modoRemocao = false;
+    btnRemoverModo.textContent = "Remove App";
+    alternarAvisoRemocao(false);
+}
+
+
+
+renderizaApps();
